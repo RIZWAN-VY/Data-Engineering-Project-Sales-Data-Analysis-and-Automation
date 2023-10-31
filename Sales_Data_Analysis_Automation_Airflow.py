dbd5036404dc1670f0310a666da12007e77ec5a1
RIZWAN-VY <rizwanyoosaf37@gmail.com>
2023-10-31 12:58:27 +0530

    SALES DATA ANALYSIS USING APACHE SPARK SQL AND AUTOMATION USING APACHE AIRFLOW

---
+++ Sales_Data_Analysis_Automation_Airflow.py
@@ -0,0 +1,12 @@
+'''
+    SALES DATA ANALYSIS USING APACHE SPARK SQL AND AUTOMATION USING APACHE AIRFLOW
+---------------------------------------------------------------------------------------
+
+create a folder in HDFS  >>  upload the downloaded data to the folder created in HDFS
+
+>>  create a table in Hive >>  upload the data from HDFS to Hive table  
+
+>>  connecting Hive and Spark and doing Analysis and saving it in ORC format  
+
+>>  upload data insight to HDFS
+'''
